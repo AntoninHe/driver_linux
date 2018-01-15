ec225217ede51f93b085a4ae9db9e8e9fdfdf047
Antonin Héréson <antonin.he@outlook.fr>
2018-01-15 15:29:15 +0100

    clean code

--- Spi.c
+++ Spi.c
@@ -16,9 +16,15 @@ static struct semaphore spi_sem;
 static short int data;
 int AXE_CONFIG=AXE_X;
 
-//#define debug(); printk(KERN_DEBUG "Line number %d, status : %x\n", __LINE__,at91_spi_read(AT91_SPI_SR)); 
+//#define debug(); printk(KERN_DEBUG "Line number %d, status : %x\n", __LINE__,at91_spi_read(AT91_SPI_SR));
 #define debug();
 
+/*****************************************************************************/
+/* Function..: spi_interrupt                                                 */
+/* Parameter.:                                                               */
+/* Var.......:                                                               */
+/* Return....:                                                               */
+/*****************************************************************************/
 static irqreturn_t spi_interrupt(int irq, void *dev_id)
 {
 	data = at91_spi_read(AT91_SPI_RDR);
@@ -26,26 +32,43 @@ static irqreturn_t spi_interrupt(int irq, void *dev_id)
 	return IRQ_HANDLED;
 }
 
+/*****************************************************************************/
+/* Function..: convert_spi_value                                             */
+/* Parameter.:                                                               */
+/* Var.......:                                                               */
+/* Return....:                                                               */
+/*****************************************************************************/
 short int convert_spi_value(short int data){
 	// Traduction de la reponse en degrés
 	short int result=0;
 	if ((data & (1<<13))==0x0000){
-		result = ((short int)(data & 0x3FFF)) /40 ;	
+		result = ((short int)(data & 0x3FFF)) /40 ;
 	}
 	else if ((data  & (1<<13))==0x2000){
-		result = ((short int)(data | 0x8000)) /40 ;	
+		result = ((short int)(data | 0x8000)) /40 ;
 	}
 	return result;
 }
 
-
+/*****************************************************************************/
+/* Function..: spiReadX                                                      */
+/* Parameter.:                                                               */
+/* Var.......:                                                               */
+/* Return....:                                                               */
+/*****************************************************************************/
 short int spiReadX()
 {
 	at91_spi_write(AT91_SPI_TDR, 0x0C00);
 	down(&spi_sem);	// Attente de la fin de transfert
-	return convert_spi_value(data);	
+	return convert_spi_value(data);
 }
 
+/*****************************************************************************/
+/* Function..: spiReadY                                                      */
+/* Parameter.:                                                               */
+/* Var.......:                                                               */
+/* Return....:                                                               */
+/*****************************************************************************/
 short int spiReadY()
 {
 	at91_spi_write(AT91_SPI_TDR, 0x0E00);
@@ -54,8 +77,14 @@ short int spiReadY()
 }
 
 
+/*****************************************************************************/
+/* Function..: spi_read                                                      */
+/* Parameter.:                                                               */
+/* Var.......:                                                               */
+/* Return....:                                                               */
+/*****************************************************************************/
 static ssize_t spi_read(struct file *file, char *buf, size_t count, loff_t *ppos)
-{	
+{
 	short int result;
 	switch (AXE_CONFIG){
 		case AXE_X :
@@ -88,6 +117,12 @@ static ssize_t spi_read(struct file *file, char *buf, size_t count, loff_t *ppos
 
 
 //int ioctl(int fd, int cmd, char *argp); user
+/*****************************************************************************/
+/* Function..: spi_ioctl                                                     */
+/* Parameter.:                                                               */
+/* Var.......:                                                               */
+/* Return....:                                                               */
+/*****************************************************************************/
 static int spi_ioctl(struct inode *inode, struct file *file, unsigned int cmd, unsigned long arg){
 	switch(cmd){
 		case SET_AXE :
@@ -95,7 +130,7 @@ static int spi_ioctl(struct inode *inode, struct file *file, unsigned int cmd, u
 				case  AXE_X:
 					AXE_CONFIG = AXE_X; // Pour X
 					break;
-			
+
 				case AXE_Y :
 					AXE_CONFIG =  AXE_Y; // Pour Y
 					break;
@@ -103,7 +138,7 @@ static int spi_ioctl(struct inode *inode, struct file *file, unsigned int cmd, u
 				case AXE_XY :
 					AXE_CONFIG =  AXE_XY; // Pour Y
 					break;
-			
+
 				default :
 					printk("axe unknown\n");
 					return -1;
@@ -118,6 +153,12 @@ static int spi_ioctl(struct inode *inode, struct file *file, unsigned int cmd, u
 }
 
 
+/*****************************************************************************/
+/* Function..: spi_open                                                      */
+/* Parameter.:                                                               */
+/* Var.......:                                                               */
+/* Return....:                                                               */
+/*****************************************************************************/
 static ssize_t spi_open(struct inode *inode, struct file *file)
 {
 	if((at91_spi_read(AT91_SPI_IMR) & AT91_SPI_RDRF) != AT91_SPI_RDRF) return -1;
@@ -127,12 +168,19 @@ static ssize_t spi_open(struct inode *inode, struct file *file)
   return 0;
 }
 
+/*****************************************************************************/
+/* Function..: spi_close                                                             */
+/* Parameter.:                                                               */
+/* Var.......:                                                               */
+/* Return....:                                                               */
+/*****************************************************************************/
 static ssize_t spi_close(struct inode *inode, struct file *file)
 {
 	printk(KERN_DEBUG "close SPI\n");
 	debug();
   return 0;
 }
+
 
 static struct file_operations fops =
 {
@@ -142,6 +190,12 @@ static struct file_operations fops =
   release : spi_close
 };
 
+/*****************************************************************************/
+/* Function..: module_spi_cleanup                                            */
+/* Parameter.:                                                               */
+/* Var.......:                                                               */
+/* Return....:                                                               */
+/*****************************************************************************/
 static int __init module_spi_init(void)
 {
 	int ret;
@@ -151,43 +205,28 @@ static int __init module_spi_init(void)
 	/*************************
 	PIO Controler setup
 	*************************/
-	temp = 	AT91_PA0_MISO 	| 	/*MISO*/
-			AT91_PA1_MOSI 	| 	/*MOSI*/
-			AT91_PA2_SPCK 	| 	/*SPCK*/
-			AT91_PA3_NPCS0;	/*NPCS0*/
-	
-	at91_sys_write(	AT91_PIOA+PIO_PDR,temp);
-	
-	//at91_sys_write(	AT91_PIOA+PIO_PDR, 	/*Pin controlled by peripheral*/
-	//				AT91_PA0_MISO 	| 	/*MISO*/
-	//				AT91_PA1_MOSI 	| 	/*MOSI*/
-	//				AT91_PA2_SPCK 	| 	/*SPCK*/
-	//				AT91_PA3_NPCS0);	/*NPCS0*/
+	at91_sys_write(	AT91_PIOA+PIO_PDR, 	/*Pin controlled by peripheral*/
+					AT91_PA0_MISO 	| 	/*MISO*/
+					AT91_PA1_MOSI 	| 	/*MOSI*/
+					AT91_PA2_SPCK 	| 	/*SPCK*/
+					AT91_PA3_NPCS0);	/*NPCS0*/
 
-	temp = 	AT91_PA0_MISO 	| 	/*MISO*/
-			AT91_PA1_MOSI 	| 	/*MOSI*/
-			AT91_PA2_SPCK 	| 	/*SPCK*/
-			AT91_PA3_NPCS0;	/*NPCS0*/
-			
-	at91_sys_write(	AT91_PIOA+PIO_ASR, temp);
-	
-	//at91_sys_write(	AT91_PIOA+PIO_ASR, 	/*Peripheral A pin select*/
-	//				AT91_PA0_MISO 	| 	/*MISO*/
-	//				AT91_PA1_MOSI 	| 	/*MOSI*/
-	//				AT91_PA2_SPCK 	| 	/*SPCK*/
-	//				AT91_PA3_NPCS0);	/*NPCS0*/
 
-			
+	at91_sys_write(	AT91_PIOA+PIO_ASR, 	/*Peripheral A pin select*/
+					AT91_PA0_MISO 	| 	/*MISO*/
+					AT91_PA1_MOSI 	| 	/*MOSI*/
+					AT91_PA2_SPCK 	| 	/*SPCK*/
+					AT91_PA3_NPCS0);	/*NPCS0*/
+
+
 
 	/*************************
 	PMC setup
 	*************************/
-	temp=1<<AT91_ID_SPI;
-	at91_sys_write(AT91_PMC_PCER,temp);
-//	at91_sys_write(	AT91_PMC_PCER,			/*Peripheral Clock Enable*/
-//					1<<AT91_ID_SPI);	/*SPI enable*/		
-	
-					
+	at91_sys_write(	AT91_PMC_PCER,			/*Peripheral Clock Enable*/
+					1<<AT91_ID_SPI);	/*SPI enable*/
+
+
 	/*************************
 	SPI setup
 	*************************/
@@ -195,28 +234,22 @@ static int __init module_spi_init(void)
 
 	at91_spi_write( AT91_SPI_CR,AT91_SPI_SWRST);	/* SPI Software Reset */
 
-	
-	temp=64<<24;
-	temp|=AT91_SPI_MSTR|AT91_SPI_PS_FIXED|AT91_SPI_PS_FIXED|AT91_SPI_MODFDIS|AT91_SPI_DIV32;
+
 	at91_spi_write(AT91_SPI_MR,temp);
-//	at91_spi_write( AT91_SPI_MR,				/* Mode Register */
-//					AT91_SPI_MSTR		|/* Master/Slave Mode */
-//					AT91_SPI_PS_FIXED   	|/* Chip select fixed*/
-//					0x00<<16		|/*NPCS 0*/
-//					64<<24			|/* AT91_SPI_DLYBCT = 64 = 11.38µs */
-//					AT91_SPI_MODFDIS	|/*mode fault detection disabled*/
-//					AT91_SPI_DIV32);	/* Clock Selection */				
-	temp =9<<16;
-	temp1=64<<24;
-	temp|=temp1 | AT91_SPI_CPOL | AT91_SPI_BITS_16 |(4<<8);
-	at91_spi_write(	AT91_SPI_CSR(0),temp);
-	
-//	at91_spi_write(	AT91_SPI_CSR(0),	/*	Chips select register 0*/
-//					AT91_SPI_CPOL		|/*	Clock Polarity*/
-//					AT91_SPI_BITS_16	|/*	16-bits transfer*/
-//					9<<16				|/* AT91_SPI_DLYBS = 9/180MHz = 50ns */
-//					64<<24				|/* AT91_SPI_DLYBCT = 64 = 11.38µs */
-//					4<<8);				/*	Baud rate MCK / (64*SCBR) SCBR=4 SPCK=703,1 kHz*/
+	at91_spi_write( AT91_SPI_MR,				/* Mode Register */
+					AT91_SPI_MSTR		|/* Master/Slave Mode */
+					AT91_SPI_PS_FIXED   	|/* Chip select fixed*/
+					0x00<<16		|/*NPCS 0*/
+					64<<24			|/* AT91_SPI_DLYBCT = 64 = 11.38µs */
+					AT91_SPI_MODFDIS	|/*mode fault detection disabled*/
+					AT91_SPI_DIV32);	/* Clock Selection */
+
+	at91_spi_write(	AT91_SPI_CSR(0),	/*	Chips select register 0*/
+					AT91_SPI_CPOL		|/*	Clock Polarity*/
+					AT91_SPI_BITS_16	|/*	16-bits transfer*/
+					9<<16				|/* AT91_SPI_DLYBS = 9/180MHz = 50ns */
+					64<<24				|/* AT91_SPI_DLYBCT = 64 = 11.38µs */
+					4<<8);				/*	Baud rate MCK / (64*SCBR) SCBR=4 SPCK=703,1 kHz*/
 
 	init_MUTEX_LOCKED(&spi_sem);
 	if (request_irq(AT91_ID_SPI, spi_interrupt,0,"at91_spi",NULL) < 0) printk(KERN_DEBUG "request_irq fault\n");
@@ -240,8 +273,14 @@ static int __init module_spi_init(void)
 	printk(KERN_DEBUG "pilote spi charge avec succes\n");
 	printk(KERN_DEBUG "MAJEUR : %d\n",ret);
 	return 0;
-} 
+}
 
+/*****************************************************************************/
+/* Function..: module_spi_cleanup                                                             */
+/* Parameter.:                                                               */
+/* Var.......:                                                               */
+/* Return....:                                                               */
+/*****************************************************************************/
 static void __exit module_spi_cleanup(void)
 {
 	int ret;
@@ -250,10 +289,9 @@ static void __exit module_spi_cleanup(void)
 	ret=unregister_chrdev(majeur,"spi_inclinometre_driver");
 	if (ret < 0)
 		printk(KERN_WARNING "erreur fonction unregister_chrdev\n");
-	else 
+	else
 		printk(KERN_DEBUG "pilote spi decharge avec succes\n");
 }
 
 module_init(module_spi_init);
 module_exit(module_spi_cleanup);
-
